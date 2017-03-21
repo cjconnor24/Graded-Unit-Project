ee37b680325a2763a1298d633783ccdb29a71a3f
Chris Connor <cjconnor24@connordesign.com>
2017-03-21 09:25:34 +0000

    Added database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $this->call(SizesTableSeeder::class);
+//        DB::table('sizes')->insert([
+//            'name' => 'A4',
+//            'width' => 210,
+//            'height' => 297,
+//        ]);
         // $this->call(UsersTableSeeder::class);
+//        DB::table('users')->insert([
+//            'name' => str_random(10),
+//            'email' => str_random(10).'@gmail.com',
+//            'password' => bcrypt('secret'),
+//        ]);
     }
 }
