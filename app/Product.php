b86c6ad7783c886034a793aa32e6c01ff844c5e1
Chris Connor <cjconnor24@connordesign.com>
2017-03-21 10:25:19 +0000

    Tweaking realtionships

--- app/Product.php
+++ app/Product.php
@@ -15,6 +15,19 @@ class Product extends Model
        return $this->belongsToMany(Size::class);
     }
 
+    /**
+     * Associate product with multiple papers
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function papers()
+    {
+        return $this->belongsToMany(Paper::class);
+    }
+
+    /**
+     * Associate product with one category
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function categories()
     {
         return $this->belongsTo(Category::class);
--- database/migrations/2017_03_20_131727_create_products_table.php
+++ database/migrations/2017_03_20_131727_create_products_table.php
@@ -18,6 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->text('description');
             $table->decimal('price');
+            $table->integer('category_id');
             $table->timestamps();
 
         });
