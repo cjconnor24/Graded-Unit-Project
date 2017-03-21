afba212c70ce30f704dc2a71a3f11372122f34ca
Chris Connor <cjconnor24@connordesign.com>
2017-03-21 13:26:14 +0000

    Tweaking the relationships

--- app/Address.php
+++ app/Address.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    /**
+     * Associates address with a particular user
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
