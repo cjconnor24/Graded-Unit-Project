51a922067f3fba09ed1cc97255cbc112152d108b
Chris Connor <cjconnor24@connordesign.com>
2017-03-27 13:43:40 +0100

    Updating view

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -11,18 +11,6 @@
     {!! Form::submit('Add Category',['class'=>'btn btn-success']) !!}
     {!! Form::close() !!}
 
-    {{--<form action="/categories" method="post" role="form">--}}
-        {{--{{csrf_field()}}--}}
-
-        {{--<div class="form-group">--}}
-            {{--<label for="name">Name</label>--}}
-            {{--<input type="text" class="form-control" name="name" id="name" placeholder="Enter Name">--}}
-        {{--</div>--}}
-
-        {{--<div class="form-group">--}}
-            {{--<button type="submit" name="submit" class="btn btn-success">Add Category</button>--}}
-        {{--</div>--}}
-
     </form>
 @include('includes.errors')
 @endsection
