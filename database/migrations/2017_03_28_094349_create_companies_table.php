49feba5f4301ca041fc80cc6cd886a1a8dc996e4
Chris Connor <cjconnor24@connordesign.com>
2017-03-28 11:04:35 +0100

    Adding company model and migration

---
+++ database/migrations/2017_03_28_094349_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('telephone');
+            $table->string('website');
+            $table->integer('main_contact_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('main_contact_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
