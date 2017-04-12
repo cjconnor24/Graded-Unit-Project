d932c873101d8c885243b502fc71652e4b98db3f
Chris Connor <cjconnor24@connordesign.com>
2017-04-12 08:36:14 +0100

    Amended User model to Extend Sentinels EloquentUser Model

--- app/User.php
+++ app/User.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use \Cartalyst\Sentinel\Users\EloquentUser;
 
-class User extends Authenticatable
+
+class User extends EloquentUser
 {
     use Notifiable;
 
@@ -15,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
