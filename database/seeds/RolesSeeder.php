2fb2aa3ef9b1e3d85e94a847067b5b9552d86b84
Chris Connor <cjconnor24@connordesign.com>
2017-04-20 12:16:39 +0100

    Roles seeder to add basic roles to DB

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert(array(
+            array('slug'=>'admin','name'=>'Administrators','created_at'=>\Carbon\Carbon::now()),
+            array('slug'=>'accounts','name'=>'Accounts','created_at'=>\Carbon\Carbon::now()),
+            array('slug'=>'customer','name'=>'Customers','created_at'=>\Carbon\Carbon::now())
+        ));
+
+    }
+}
