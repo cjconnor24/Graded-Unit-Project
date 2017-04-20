5cb244debd6af935681fdd742134e056316b9c9d
Chris Connor <cjconnor24@connordesign.com>
2017-04-20 12:17:00 +0100

    Adding Admin middleware to restrict admin routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'authenticate' => \App\Http\Middleware\AuthenticateMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
