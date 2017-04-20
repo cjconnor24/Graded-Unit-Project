023436d5a4a3225e7b1a09eefc6cca574ca2b01e
Chris Connor <cjconnor24@connordesign.com>
2017-04-20 12:17:25 +0100

    Adding roles seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(SizesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(PapersTableSeeder::class);
+        $this->call(RolesSeeder::class);
 //        factory(App\User::class, 100)->create();
 //        factory(App\Address::class)->create();
 
