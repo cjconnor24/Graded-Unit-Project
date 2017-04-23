73ef94580c44e80ecfa5bcc6d2aea038073a9329
Chris Connor <cjconnor24@connordesign.com>
2017-04-23 20:34:50 +0100

    Trying new doc generator

--- config/app.php
+++ config/app.php
@@ -183,10 +183,6 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         // ADDED FOR SENTINEL
         Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
-        /*
-         * ADDED FOR API GENERATOR DOC
-         */
-        Mpociot\ApiDoc\ApiDocGeneratorServiceProvider::class,
 
     ],
 
