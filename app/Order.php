a19078a52176e185b938631db2c6fd08e1656c25
Chris Connor <cjconnor24@connordesign.com>
2017-04-24 13:35:16 +0100

    Order was missing many-to-many relationship with product

--- app/Order.php
+++ app/Order.php
@@ -13,7 +13,7 @@ class Order extends Model
      */
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsToMany(Product::class);
     }
 
     /**
