ec2997228108e988ef97c8a4ce952a52dd50b1c5
Chris Connor <cjconnor24@connordesign.com>
2017-04-25 12:06:14 +0100

    Testing controller directory - reverted

--- app/Http/Controllers/PaperController.php
+++ app/Http/Controllers/PaperController.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 
-use App\Http\Controllers\Controller;
 use \App\Paper;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
