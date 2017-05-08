bd057caff7b0a141f3b0c1afbcbcb8596ffa25df
Chris Connor <chris@chrisconnor.co.uk>
2017-05-08 15:31:31 +0100

    Ensuring a user can only access their own quotes

--- app/Http/Middleware/QuoteOwner.php
+++ app/Http/Middleware/QuoteOwner.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Order;
+use Sentinel;
 use Closure;
 
 class QuoteOwner
@@ -15,25 +17,43 @@ class QuoteOwner
      */
     public function handle($request, Closure $next)
     {
-        $id = $request->route('quotation')->id;
+        $id = $request->route('quotation');
 
-        dd($id);
 
-        $address = Address::findOrFail($id);
+        $order = Order::findOrFail($id);
         $user = Sentinel::getUser();
 
-        // CHECK AUTHENTICATED USER IS THE OWNER
-        if($address->user_id == $user->id)
-        {
 
-            return $next($request);
 
-        } else {
 
-            return redirect()
+        if($order->customer->id !== $user->id){
+
+                        return redirect()
                 ->action('UserProfileController@viewAddresses')
                 ->with('error','You do not have permission to access this resource.');
 
+        } else {
+            return $next($request);
         }
+
+//
+//        return $id;
+//        dd($id);
+
+
+
+        // CHECK AUTHENTICATED USER IS THE OWNER
+//        if($address->user_id == $user->id)
+//        {
+
+
+
+//        } else {
+//
+//            return redirect()
+//                ->action('UserProfileController@viewAddresses')
+//                ->with('error','You do not have permission to access this resource.');
+//
+//        }
     }
 }
