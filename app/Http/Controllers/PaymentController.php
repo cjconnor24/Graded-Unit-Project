e989c0f5035ae9b6ef62a46f7d178e683df5b264
Chris Connor <chris@chrisconnor.co.uk>
2017-05-09 18:23:51 +0100

    Adding additional logic to store Braintree Response

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,24 +10,68 @@ class PaymentController extends Controller
 
     public function index(Order $order)
     {
+        $order->load('customer','address');
 
+//        return $order->address->toArray();
+//        return response()->json($order);
         return view('userviews.payment.view')->with('order',$order);
     }
 
     public function checkout(Order $order, Request $request)
     {
+        $order->load('customer','address');
+        $customer = $order->customer;
+
+        $address = $order->address->toArray();
+
+//        dd($address);
+
         $nonceFromTheClient = $request->payment_method_nonce;
 
 
         $result = \Braintree_Transaction::sale([
             'amount' => $order->order_total,
+            'orderId'=> "$order->id",
+            'merchantAccountId' => 'gbp_account',
+//            'customerId'=>$order->customer->id,
             'paymentMethodNonce' => $nonceFromTheClient,
+            'customer' => [
+                'firstName' => $customer->first_name,
+                'lastName' => $customer->last_name,
+                'phone' => '312-555-1234',
+                'fax' => '312-555-1235',
+                'email' => $customer->email
+            ],
             'options' => [
                 'submitForSettlement' => True
             ]
         ]);
 
-        return response()->json($result);
+//        dd($result);
+        $transaction = $result->transaction;
+//        dd([$result,$transaction]);
+
+
+
+
+
+
+        if($result->success){
+            $order->payments()->create([
+                'customer_id'=>$customer->id,
+                'transaction_id'=>$transaction->id,
+                'amount'=>$transaction->amount,
+                'success'=>$result->success,
+            ]);
+            return response()->json($result);
+
+        } else {
+
+            return response()->json($result);
+
+        }
+
+
     }
 
 }
--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = ['customer_id',
+        'order_id',
+        'transaction_id',
+        'amount',
+        'braintree_response',
+        'success'];
+    // RELATIONSHIPS
+
+    /**
+     * Payment belongs to an orders
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * Payment belongs to a customer
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function customer()
+    {
+        return $this->belongsTo(User::class,'customer_id');
+    }
+
 }
