693793335cdf6bc553daa3453ff3cffbc467905f
Chris Connor <cjconnor24@connordesign.com>
2017-06-08 16:34:34 +0100

    Changing integers

--- database/seeds/SizesTableSeeder.php
+++ database/seeds/SizesTableSeeder.php
@@ -17,9 +17,9 @@ class SizesTableSeeder extends Seeder
             array('name'=>'A2','height'=>420,'width'=>594,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
             array('name'=>'A3','height'=>297,'width'=>420,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
             array('name'=>'A4','height'=>210,'width'=>297,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
-            array('name'=>'A5','height'=>148.5,'width'=>210,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
-            array('name'=>'A6','height'=>105,'width'=>148.5,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
-            array('name'=>'A7','height'=>74.25,'width'=>105,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
+            array('name'=>'A5','height'=>148,'width'=>210,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
+            array('name'=>'A6','height'=>105,'width'=>148,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
+            array('name'=>'A7','height'=>74,'width'=>105,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
             array('name'=>'Business Card (85 x 55)','height'=>55,'width'=>85,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47'),
             array('name'=>'Business Card (90 x 55)','height'=>55,'width'=>90,'created_at'=>'2017-03-21 07:41:47','updated_at'=>'2017-03-21 07:41:47')
         ));
